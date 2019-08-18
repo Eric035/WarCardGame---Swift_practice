8ecb3f23c5bcc6459192bba1fb102619a6128d4c
Eric035 <ericleung1997@gmail.com>
2019-08-18 15:58:48 -0400

    ios dev tutorial. Practice Swift

---
+++ WarCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCard
+//
+//  Created by Eric Leung on 2019-08-17.
+//  Copyright © 2019 Eric Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
